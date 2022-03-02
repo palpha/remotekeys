d64bb220a905481f270df84bb8ed24e3e6ab4c6d
Niklas Bergius <niklas@bergius.org>
2022-03-02 22:24:58 +0100

    Made much more insanely insecure.

--- StreamDeckHelper/Program.cs
+++ StreamDeckHelper/Program.cs
@@ -10,7 +10,7 @@ Config ReadConfig() =>
 InputSimulator input = new();
 var config = ReadConfig();
 
-IEnumerable<VirtualKeyCode> ParseModifier( string[] keys )
+IEnumerable<VirtualKeyCode> ParseModifiers( string[] keys )
 {
 	foreach ( var key in keys )
 	{
@@ -26,26 +26,27 @@ IEnumerable<VirtualKeyCode> ParseModifier( string[] keys )
 	}
 }
 
-VirtualKeyCode ParseFKey( string str ) =>
+VirtualKeyCode ParseKey( string str ) =>
 	str.ToUpperInvariant() switch
 		{
 			var x when x[0] == 'F' => Enum.Parse<VirtualKeyCode>( x ),
-			var x => throw new InvalidOperationException( $"Invalid F-key {x}." )
+			var x when x.Length == 1 => Enum.Parse<VirtualKeyCode>( $"VK_{x}" ),
+			"ESC" => VirtualKeyCode.ESCAPE,
+			"ESCAPE" => VirtualKeyCode.ESCAPE,
+			var x => throw new InvalidOperationException( $"Invalid key {x}." )
 		};
 
-bool LoggedKeyPress( VirtualKeyCode key )
+void LoggedKeyPress( VirtualKeyCode key )
 {
 	Console.WriteLine( $"Key: {key}." );
 	input.Keyboard.KeyPress( key );
-	return true;
 }
 
-bool LoggedModifiedKeyStroke( IEnumerable<VirtualKeyCode> modifiers, VirtualKeyCode key )
+void LoggedModifiedKeyStroke( IEnumerable<VirtualKeyCode> modifiers, VirtualKeyCode key )
 {
 	var modifierKeyCodes = modifiers.ToList();
 	Console.WriteLine( $"Modifiers: {string.Join( ", ", modifierKeyCodes )}; key: {key}." );
 	input.Keyboard.ModifiedKeyStroke( modifierKeyCodes, key );
-	return true;
 }
 
 bool LoggedTextEntry( string str )
@@ -55,15 +56,46 @@ bool LoggedTextEntry( string str )
 	return true;
 }
 
+bool ParseKeys( string[] keyPresses )
+{
+	var sequence = new List<Action>();
+
+	foreach ( var keyPress in keyPresses )
+	{
+		if ( int.TryParse( keyPress, out var pause ) && pause is > 0 and < 10000 )
+		{
+			Console.WriteLine( $"Pausing {pause} ms." );
+			Thread.Sleep( pause );
+			continue;
+		}
+
+		var parts = keyPress.Split( '+' );
+		if ( parts.Length == 1 )
+		{
+			var key = ParseKey( parts[0] );
+			sequence.Add( () => LoggedKeyPress( key ) );
+		}
+		else
+		{
+			var modifiers = ParseModifiers( parts[..^1] );
+			var key = ParseKey( parts.Last() );
+			sequence.Add( () => LoggedModifiedKeyStroke( modifiers, key ) );
+		}
+	}
+
+	foreach ( var action in sequence )
+	{
+		action();
+	}
+
+	return true;
+}
+
 bool Parse( string[] command ) =>
 	command[0] switch
 		{
 			"text" when config.EnableText => LoggedTextEntry( command[1] ),
-			"f" when command.Length == 2 => LoggedKeyPress( ParseFKey( command[1] ) ),
-			"f" when command.Length >= 3 =>
-				LoggedModifiedKeyStroke(
-					ParseModifier( command[1..^1] ).Distinct(),
-					ParseFKey( command.Last() ) ),
+			"keys" => ParseKeys( command[1..] ),
 			_ => false
 		};
 
